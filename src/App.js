a3e47475ee6462044941e54ddbc4e0c247ff35f2
saurabh4380 <saurabhumadikar@gmail.com>
2022-05-16 08:52:40 +0530

    Initial Commit Updating the App to use Directory and Category-Item components to show the categories.

--- src/App.js
+++ src/App.js
@@ -1,36 +1,36 @@
+import CategoryItem from './components/category-item/category-item.component';
+import Directory from './components/directory/directory.component';
+
 function App() {
   const categories = [
     {
-      id : 1,
-      title : 'Hats'
+      id: 1,
+      title: "hats",
+      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
     },
     {
-      id : 2,
-      title : 'Jackets'
+      id: 2,
+      title: "jackets",
+      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
     },
     {
-      id : 3,
-      title : 'Sneakers'
+      id: 3,
+      title: "sneakers",
+      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
     },
     {
-      id : 4,
-      title : 'Women'
+      id: 4,
+      title: "womens",
+      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
     },
     {
-      id : 5,
-      title : 'Men'
+      id: 5,
+      title: "mens",
+      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
     },
-  ]
+  ];
   return (
-    <div className="categories-container">
-
-      {categories.map(({title, id}) => ( <div className="category-container" key={id}>
-        <div className="category-body-container">
-          <h2>{title}</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>))}
-    </div>
+    <Directory categories={categories}/>
   );
 }
 
